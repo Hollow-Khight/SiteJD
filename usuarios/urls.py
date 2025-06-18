449837a4b5397168305cefec2834eb72993f1d72
Hollow-Khight <jeshuahung936@gmail.com>
2025-06-17 21:37:00 -0400

    Atualização 17/06/2025

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name = 'login'),
+    path('cadastro/', views.cadastro, name = 'cadastro'),
+    path('home/', views.home,name = 'home'),
+    path('lancar/', views.lancar,name = 'lancar'),
+    path('alterar/', views.alterar,name = 'alterar'),
+    path('visualizar/', views.visualizar,name = 'visualizar'),
+    path('logout/', views.logout, name = 'logout'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
+from django.contrib.auth import login as login_django, logout as logout_django
 
 def login(request):
     if request.method == "GET":
@@ -15,6 +15,56 @@ def login(request):
 
         if user:
             login_django(request, user)
-            return HttpResponse('Autenticado!')
+            return render(request, 'usuarios/home.html')
         else:
             return HttpResponse('Email ou senha inválidos!')
+
+def logout(request):
+    if request.user.is_authenticated:
+        logout_django(request)
+        return render(request, 'usuarios/login.html')
+    else:
+        return HttpResponse("Você não acessou sua conta ainda!")
+        
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'usuarios/cadastro.html')
+    else:
+        username  = request.POST.get('email')
+        email = request.POST.get('email')
+        password = request.POST.get('senha')
+        first_name =  request.POST.get('nome')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse("Usuario já existente!")
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name)
+            user.save()
+            return render (request, 'usuarios/login.html')
+
+        
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/home.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
+
+def lancar(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/lancar.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
+
+def alterar(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/alterar.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
+
+def visualizar(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/visualizar.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
